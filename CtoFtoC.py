fd91d0c69954dea33920eb96bcbb170c581e9d23
SyuanEmon <muska1116@gmail.com>
2020-03-20 13:03:46 +0800

    Update CtoFtoC

--- CtoFtoC.py
+++ CtoFtoC.py
@@ -1,5 +1,5 @@
-f = float(input("請輸入華氏溫度:"))
-c = float(input("請輸入攝氏溫度:"))
+f = float(input("請輸入°F:"))
+c = float(input("請輸入°C:"))
 
-print("攝氏溫度為:%.2f" % ((f - 32) * 5 / 9))
-print("華氏溫度為:%.2f" % (c * 1.8 + 32))
+print("°C為:%.2f" % ((f - 32) * 5 / 9))
+print("°F為:%.2f" % (c * 1.8 + 32))
