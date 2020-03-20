d44a9e29c208bfa0f181e246463b74a791705478
SyuanEmon <muska1116@gmail.com>
2020-03-20 15:20:41 +0800

    Update mora

--- mora.py
+++ mora.py
@@ -1,7 +1,7 @@
 #randon=隨機數工具包MOD模組
 import random
 
-i = int(input("請輸入要玩幾次"))
+i = int(input("請輸入要玩幾次:"))
 a = 0
 play_win = 0
 computer_win = 0
@@ -27,13 +27,9 @@ while a < i:
         print("電腦出石頭結果")
     elif computer == 3:
         print("電腦出布結果")
-
-
-
 #石頭贏剪刀
 #剪刀贏布
 #布贏石頭
-
     if ((play == 1 and computer == 3)
             or (play == 2 and computer == 1)
             or (play == 3 and computer == 2)):
@@ -47,5 +43,4 @@ while a < i:
         print("***電腦贏了***")
         computer_win += 1
     a += 1
-
 print("總共比了%d場玩家贏了%d場，電腦贏了%d場，平手%d場，勝率%.2f%%" % (i, play_win, computer_win, play_computer, play_win/i*100))
