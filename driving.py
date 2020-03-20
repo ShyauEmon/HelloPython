eed32de030342540ac0691baa0376ea061590ddd
SyuanEmon <muska1116@gmail.com>
2020-03-20 18:03:06 +0800

    NEW driving

---
+++ driving.py
@@ -0,0 +1,14 @@
+county = input("請輸入國家")
+age = int(input("請輸入年齡"))
+
+if county == "台灣":
+    if age < 18:
+        print("不能考駕照")
+    else:
+        print("妳可以考駕照")
+
+elif county == "美國":
+    if age < 16:
+        print("不能考駕照")
+    else:
+        print("妳可以考駕照")
