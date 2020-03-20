ef6c074302ff24c20052e962a616791e8ba1b737
SyuanEmon <muska1116@gmail.com>
2020-03-20 18:47:43 +0800

    Update pd

--- pd.py
+++ pd.py
@@ -1,13 +1,14 @@
-pd_01 = "a1234567"
-a = 3
-while a > 0:
-    pd = input("請輸入密碼可輸入%d次:" %a)
+pd_01 = "a123456"
+i = 3
+while i > 0:
+	i -= 1
+    pd = input("請輸入密碼:")
     if pd == pd_01:
         print("密碼正確登入中")
         break
-    elif pd != pd_01:
-        print("密碼錯誤剩餘嘗試%d次" % (a-1))
-        a -= 1
-
-if a == 0:
-    print("密碼連續錯誤請15分後再試")
+    else:
+        print("密碼錯誤")
+        if i > 0:
+        	print("剩餘嘗試%d次" % (i))
+        else
+        	print("密碼連續錯誤請15分後再試")
